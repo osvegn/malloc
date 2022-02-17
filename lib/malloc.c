64e0519718c57229244a5ec9a96ddcd6eda23920
Thomas <thoprudhomme@gmail.com>
2022-02-17 14:46:07 +0100

    NEW : mutex

--- lib/malloc.c
+++ lib/malloc.c
@@ -7,19 +7,21 @@
 
 #include "lib_malloc.h"
 #include <unistd.h>
-#include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
 
-void *malloc(size_t size)
+void *my_malloc(size_t size)
 {
     block_t *block = NULL;
     size_t i = 0;
+    pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
 
+    pthread_mutex_lock(&mut);
     for (i = 1; i < size; i *= 2);
     size = i;
     if (!get_first_block()) {
         block = create_first_block(size);
+        pthread_mutex_unlock(&mut);
         return (block->allocated);
     }
     block = find_free_space(size);
@@ -28,5 +30,15 @@ void *malloc(size_t size)
         if (!block)
             return (NULL);
     }
+    pthread_mutex_unlock(&mut);
     return (block->allocated);
+}
+
+void *malloc(size_t size)
+{
+    void *ptr = NULL;
+    int pthreads_create();
+
+    ptr = my_malloc(size);
+    return (ptr);
 }
